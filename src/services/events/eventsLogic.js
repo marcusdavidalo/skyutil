aa4d67c5a970d8e06d14820833a83c7303223fc9
Marcus David Alo <marcusdavidalo@gmail.com>
2024-06-25 11:08:20 +0800

    Updated getNextEvents function to include event end time and sort by end time

--- src/services/events/eventsLogic.js
+++ src/services/events/eventsLogic.js
@@ -1,12 +1,14 @@
 import { eventDefinitions } from "./eventData";
 import { getEventOffset } from "./eventTimeOffset";
+import { DateTime } from "luxon";
 
-// Function to get all events with their next occurrence
 export function getNextEvents(currentDate) {
   const groupedEvents = {};
 
   Object.values(eventDefinitions).forEach((eventData) => {
     const offset = getEventOffset(eventData, currentDate);
+    const startTime = DateTime.fromJSDate(offset.date);
+    const endTime = startTime.plus({ minutes: eventData.duration || 0 });
 
     const eventType = eventData.type;
     if (!groupedEvents[eventType]) {
@@ -16,14 +18,13 @@ export function getNextEvents(currentDate) {
     groupedEvents[eventType].push({
       ...eventData,
       ...offset,
+      endTime,
     });
   });
 
-  // Sort events in each group by total seconds to next event
+  // Sort events in each group by end time
   Object.values(groupedEvents).forEach((events) => {
-    events.sort(
-      (a, b) => a.totalSecondsToNextEvent - b.totalSecondsToNextEvent
-    );
+    events.sort((a, b) => a.endTime.toMillis() - b.endTime.toMillis());
   });
 
   return groupedEvents;
